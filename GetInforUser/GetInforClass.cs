3ff726712cf82c1be624f03bbffc6b8395da7b95
douglas ruan de araujo fernando <67982779+Douglasghs@users.noreply.github.com>
2021-10-05 17:43:24 -0300

    Parte de captura de informação completa
    Mais o método de pegar todos os valores

--- GetInforUser/GetInforClass.cs
+++ GetInforUser/GetInforClass.cs
@@ -13,9 +13,6 @@ namespace GetInforUser
     class GetInforClass
     {
         string urlCaminhoArquivoLog = "@/Users/Ruann/OneDrive/Documentos/GitHub/GetInfor-1-2---Service-Windows/Configurações/ARQUIVO DE LOG.txt";
-        string IpAndress;
-        string userName;
-        string hostName;
 
         private string GetIpAndressIPV4()
         {
@@ -94,15 +91,32 @@ namespace GetInforUser
         private string GetUserName()
         {
             string Userwindows = "";
-            string DomainName = System.Environment.UserDomainName;
-            string AccountName = System.Environment.UserName.ToLower();
-            SelectQuery query = new SelectQuery("select FullName from Win32_UserAccount where domain='" + DomainName + "' and name='" + AccountName + "'");
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
-            foreach (ManagementBaseObject disk in searcher.Get())
+            
+            try
             {
-                Userwindows = disk["FullName"].ToString();
+                string DomainName = System.Environment.UserDomainName;
+                string AccountName = System.Environment.UserName.ToLower();
+                SelectQuery query = new SelectQuery("select FullName from Win32_UserAccount where domain='" + DomainName + "' and name='" + AccountName + "'");
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+                foreach (ManagementBaseObject disk in searcher.Get())
+                {
+                    Userwindows = disk["FullName"].ToString();
+                }
+                return Userwindows;
             }
-            return Userwindows;
+            catch (Exception e)
+            {
+                StreamWriter vWriter = new StreamWriter(urlCaminhoArquivoLog, true);
+
+                vWriter.WriteLine("----------   Serviço Método GetUserName   ----------");
+                vWriter.WriteLine(DateTime.Now.ToString());
+                vWriter.WriteLine("Source : " + e.Source);
+                vWriter.WriteLine("Message : " + e.Message);
+                vWriter.Flush();
+                vWriter.Close();
+
+                return Userwindows;
+            }
         }
 
         private string GetHostName()
@@ -129,18 +143,63 @@ namespace GetInforUser
 
         private string GetMemoryRAM()
         {
-            string Query = "SELECT Capacity FROM Win32_PhysicalMemory";
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher(Query);
-
-            UInt64 Capacity = 0;
-            foreach (ManagementObject WniPART in searcher.Get())
+            try
             {
-                Capacity += Convert.ToUInt64(WniPART.Properties["Capacity"].Value);
-            }
+                string Query = "SELECT Capacity FROM Win32_PhysicalMemory";
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(Query);
 
-            return Capacity.ToString();
+                UInt64 Capacity = 0;
+                foreach (ManagementObject WniPART in searcher.Get())
+                {
+                    Capacity += Convert.ToUInt64(WniPART.Properties["Capacity"].Value);
+                }
+
+                return Capacity.ToString();
+            }
+            catch(Exception e)
+            {
+                StreamWriter vWriter = new StreamWriter(urlCaminhoArquivoLog, true);
+
+                vWriter.WriteLine("----------   Serviço Método GetMemoryRAM   ----------");
+                vWriter.WriteLine(DateTime.Now.ToString());
+                vWriter.WriteLine("Source : " + e.Source);
+                vWriter.WriteLine("Message : " + e.Message);
+                vWriter.Flush();
+                vWriter.Close();
+
+                return "";
+            } 
         }
 
+
+        public string[] GetAllValues()
+        {
+            string[] valores = new string[5];
+
+            try
+            {
+                valores[0] = this.GetIpAndressIPV4();
+                valores[0] = this.GetIpAndressIPV6();
+                valores[0] = this.GetUserName();
+                valores[0] = this.GetHostName();
+                valores[0] = this.GetMemoryRAM();
+
+                return valores;
+            }
+            catch (Exception e)
+            {
+                StreamWriter vWriter = new StreamWriter(urlCaminhoArquivoLog, true);
+
+                vWriter.WriteLine("----------   Serviço Método GetAllValue   ----------");
+                vWriter.WriteLine(DateTime.Now.ToString());
+                vWriter.WriteLine("Source : " + e.Source);
+                vWriter.WriteLine("Message : " + e.Message);
+                vWriter.Flush();
+                vWriter.Close();
+
+                return valores;
+            }
+        }
        
     }
 }
